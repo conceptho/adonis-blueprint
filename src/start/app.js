286426a63b1872162ecd1cc7f3f23ecf82a04dfd
savioserra <serra.santos.s@gmail.com>
2019-03-01 21:32:10 -0300

    Added @conceptho/adonis-validator

--- src/start/app.js
+++ src/start/app.js
@@ -20,7 +20,8 @@ const providers = [
   '@adonisjs/session/providers/SessionProvider',
   '@adonisjs/auth/providers/AuthProvider',
   '@adonisjs/validator/providers/ValidatorProvider',
-  '@conceptho/adonis-service-layer'
+  '@conceptho/adonis-service-layer',
+  '@conceptho/adonis-validators'
 ]
 
 /*
